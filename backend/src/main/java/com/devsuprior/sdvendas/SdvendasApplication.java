db913e00d82d2a609ea171c3ff41d2a81c42c351
valmar Silva <valmar-ssilva@hotmail.com>
2021-11-06 12:20:06 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuprior/sdvendas/SdvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuprior.sdvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdvendasApplication.class, args);
+	}
+
+}
